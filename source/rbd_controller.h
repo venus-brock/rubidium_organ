6ad515792e7f255491ab3d6d8bd29ced2ae629b1
venus brock <venus_brock@venusmusic.ca>
2025-10-20 15:04:04 -0500

    added license section to source files

--- source/rbd_controller.h
+++ source/rbd_controller.h
@@ -1,3 +1,22 @@
+/*	Rubidium Organ - A simple additive synthesizer using VST3
+*	Copyright (C) 2025 Venus Brock
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU General Public License as published by
+*	the Free Software Foundation, either version 3 of the License, or
+*	(at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU General Public License for more details.
+*
+*	You should have received a copy of the GNU General Public License
+*	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+*	 Contact me at venus_brock@venusmusic.ca
+*/
+
 #pragma once
 
 #include "public.sdk/source/vst/vsteditcontroller.h"
